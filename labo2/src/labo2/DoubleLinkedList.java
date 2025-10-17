97170af7b8fb17601fb5e5e9c72f6855dbffb1f2
bo <bye001@ikasle.ehu.eus>
2025-10-17 17:33:39 +0200

    DoubleLinedList *bukatuta* baina errepasatu eta probatu gabe

--- labo2/src/labo2/DoubleLinkedList.java
+++ labo2/src/labo2/DoubleLinkedList.java
@@ -26,36 +26,63 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	  return deskr;
 	}
 
-
-	public T removeFirst() {
-		if (isEmpty()) return null;
-		Node<T> oraingoa = last;
-		while (oraingoa.prev != null) {
-			oraingoa = oraingoa.prev;
+	public void setLast(Node<T> elem) {
+		this.last= elem;
+	}
+	public void addToRear(T elem) {
+		Node<T> nodoBerria = new Node<T>(elem);
+		
+		if (isEmpty()) {
+			last=nodoBerria;
 		}
-		T kendutakoDatua = oraingoa.data;
-		if(oraingoa.next !=null) {
-			oraingoa.next.prev = null;
+		else {
+			last.next=nodoBerria;
+			nodoBerria.prev=last;
+			last=nodoBerria;
+		}
+		}
+	
+	public T removeFirst() {
+		// listako lehen elementua kendu da
+		// Aurrebaldintza: 
+			// KODEA OSATU ETA KOSTUA KALKULATU
+		if (isEmpty()) return null;
+		Node<T> current = last;
+		while (current.prev != null) {
+			current = current.prev;
+		}
+		T kendutakoDatua = current.data;
+		if(current.next !=null) {
+			current.next.prev = null;
 		} else {
 			last = null;
 		}
-		if(oraingoa == last) {
+		if(current == last) {
 			last = null;
 		}
 		count--;
 		return kendutakoDatua;
-			
-	// listako lehen elementua kendu da
-	// Aurrebaldintza: 
-		// KODEA OSATU ETA KOSTUA KALKULATU
-		
 	}
 
 	public T removeLast() {
+		// listako azken elementua kendu da
+		// Aurrebaldintza: 
+			// KODEA OSATU ETA KOSTUA KALKULATU
+		if(isEmpty()) return null;
+		Node<T> current = last;
+		T kendutakoDatua = last.data;
+		if(current.prev!=null) {
+			current.prev.next =null;
+			last=current.prev.next;
+		}
+		else {
+			last=null;
+		}
+		count--;
+		return kendutakoDatua;
 		
-	// listako azken elementua kendu da
-	// Aurrebaldintza: 
-		// KODEA OSATU ETA KOSTUA KALKULATU
+		
+	
     }
 
 
@@ -63,52 +90,139 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	// Aurrebaldintza: 
 	// Balio hori listan baldin badago, bere lehen agerpena ezabatuko dut. Kendutako objektuaren erreferentzia 
         //  bueltatuko du (null ez baldin badago)
-
 		// KODEA OSATU ETA KOSTUA KALKULATU
+		if(isEmpty()) return null;
+		T kendutakoDatua = null;
+		Node<T> current = last;
+		while(current.prev != null) {
+			current= current.prev;
+		}
+		while(current.next != null && kendutakoDatua==null) {
+			if (current.data == elem) {
+				kendutakoDatua= current.data;
+				if(current.prev != null) {
+					if(current.next != null) {
+					current.next.prev=current.prev;
+					current.prev.next=current.next;}
+					else {
+						current.prev.next=null;
+					}
+				
+				}
+				else {
+					current.next.prev=null;
+					
+				}
+			}
+			current=current.next;
+		}
+		return kendutakoDatua;
         };
 		
 	public void removeAll(T elem) {
+		
 	// Aurrebaldintza: 
 	// Balio zehatz baten agerpen guztiak ezabatzen ditu
 	
 		// KODEA OSATU ETA KOSTUA KALKULATU
-    };
+		if(!isEmpty()) {
+		Node<T> current = last;
+		while(current.prev != null) {
+			if (current.data == elem) {
+				if(current.prev != null) {
+					if(current.next != null) {
+					current.next.prev=current.prev;
+					current.prev.next=current.next;}
+					else {
+						current.prev.next=null;
+					}
+				}
+				else {
+					current.next.prev=null;
+				}
+			}
+			current =current.prev;
+		}
+		}
+		}
+   
 
 	public T first() {
-	// listako lehen elementua ematen du
-	   // KODEA OSATU ETA KOSTUA KALKULATU
+		// listako lehen elementua ematen du
+		   // KODEA OSATU ETA KOSTUA KALKULATU
+		if (isEmpty()) return null;
+		Node<T> oraingoa = last;
+		while (oraingoa.prev != null) {
+			oraingoa = oraingoa.prev;
+		}
+		return oraingoa.data;
+	
 	}
 
 	public T last() {
-	// listako azken elementua ematen du
-	   // KODEA OSATU ETA KOSTUA KALKULATU
-	}
+		// listako azken elementua ematen du
+		   // KODEA OSATU ETA KOSTUA KALKULATU
+		return last.data;
+		}
 
 	public DoubleLinkedList<T> clone(){
 		// Zerrendaren kopia bat itzultzen du (ez du punteroa bikoizten)
-	   // KODEA OSATU ETA KOSTUA KALKULATU		
+	   // KODEA OSATU ETA KOSTUA KALKULATU
+		
+		DoubleLinkedList<T> kopia = new DoubleLinkedList<T>();
+		Node<T> current = last;
+		while (current.prev != null) {
+			kopia.setLast(current);
+			current=current.prev;
+		}
+		return kopia;
 	} 
 
 	public boolean contains(T elem) {
 	// Egiazkoa bueltatuko du aurkituz gero, eta false bestela
+
+  		// KODEA OSATU ETA KOSTUA KALKULATU
+		boolean aurkitua=false;
 		      if (isEmpty())
 		          return false;
+		      Node<T> current= last;
+		      while(current.prev!=null && !aurkitua) {
+		    	  if (current.data == elem) {
+		    		  aurkitua=true;
+		    		  
+		    	  }
+		      }
+		      return aurkitua;
 
-		      		// KODEA OSATU ETA KOSTUA KALKULATU
 		   }
 
 	public T find(T elem) {
 	// Elementua bueltatuko du aurkituz gero, eta null bestela
 
 		// KODEA OSATU ETA KOSTUA KALKULATU
+		Node<T> oraingoa = last;
+		T datua = null;
+		while(oraingoa.prev!=null && datua==null) {
+			if (oraingoa.data == elem) {
+				datua=oraingoa.data;
+			}
+			
+		}
+		return datua;
 	}
 
 	public boolean isEmpty() { 
 	// KODEA OSATU ETA KOSTUA KALKULATU
+		boolean hutsik=false;
+		if (last==null) {
+			hutsik=true;
+		}
+		return hutsik;
 	}
 	
 	public int size() { 
 	// KODEA OSATU ETA KOSTUA KALKULATU
+	return count;
 	}
 	
 	/** Return an iterator to the stack that iterates through the items . */ 
@@ -116,8 +230,23 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 
 	   // an iterator, doesn't implement remove() since it's optional 
 	   private class ListIterator implements Iterator<T> { 
+		   private Node<T> current = last; 
+
+		   public boolean hasNext()  { 
+			   return (current != null);                     
+			   } 
+
+		   
+
+		   public T next() { 
+			   if (!hasNext()) return null; 
+			   T elem = current.data; 
+			   current = current.prev; 
+			   return elem; 
+			   }
 
 		// KODEA OSATU 
+		   
 	   } // private class
 		
 		
